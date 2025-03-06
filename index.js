ef257c1301ddbac267dbeadcd6af4eb4bc77296f
sudeis <ahmedsudeis24@gmail.com>
2025-03-06 11:42:59 +0300

    Add fare calculation and display functions to index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+// function to calculate fare
+function calculateFare(distance) {
+  const baseFare = 150; // base fare
+  const rateperkm = 0.5; // rate per km
+
+  // calculate the total fare
+  const totalFare = baseFare + (distance * rateperkm);
+
+// return the total fare
+  return totalFare;
+}
+//function to display the total fare
+function displayTotalFare(distance) {
+  const fare = calculateFare(distance);
+  console.log(`The Fare For Your Trip Is: ${distance} km ride is $${fare}`);
+} 
